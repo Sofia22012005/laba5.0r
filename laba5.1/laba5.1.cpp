ade428a33ae1dd5ab35413c03f3b9f8ed3d975d1
Sofia <kulchitskasofia@gmail.com>
2022-11-09 13:50:39 +0200

    second test

---
+++ laba5.1/laba5.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double f(const double a, const double b, const double c); 
+int main()
+{
+	double s, t;
+		cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double c = (f(1, t*t, s) + f(t, s*s,1))/(1+pow(f(1,t*s,1),2));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double f(const double a, const double b, const double c) 
+{
+	return (sin(a*b*c)*sin(a*b*c)/(a*a+b*b+c*c));
+}
